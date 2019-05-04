603f65f00fd785db981ebd986ef616333018671f
radiium <remip.dev@gmail.com>
2019-05-04 19:06:55 +0200

    Add app file

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+
+import store from './store/index';
+import App from './layout/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.scss';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+
+
+
+ReactDOM.render((
+    <Provider store={store}>
+        <App />
+    </Provider>),
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
