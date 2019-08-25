ecb2bcd4f50e19f58c6944a6fc670a927f5b34ee
radiium <remip.dev@gmail.com>
2019-08-26 01:41:20 +0200

    Fix html & css

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -4,12 +4,17 @@ import React, { FC } from 'react';
 // import Content from './Content/Content';
 import './App.scss';
 import PictureViewer from 'components/PictureViewer/PictureViewer';
+import Settings from 'components/Settings/Settings';
 
 const App: FC = () => {
   return (
     <div className="app-wrapper">
 
+        <Settings></Settings>
+
+        <div className="app-content">
         <PictureViewer />
+        </div>
 
         {/*
         <Header />
