c5a951fdd969e9cb2a5c34dc082272e72582e101
radiium <remip.dev@gmail.com>
2019-09-23 17:48:44 +0200

    Feat/ implement sidebar

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,28 +1,19 @@
 import React, { FC } from 'react';
 
 import Header from './Header/Header';
-import SidebarLeft from './Sidebar/SidebarLeft';
-import SidebarRight from './Sidebar/SidebarRight';
+import BrowserSidebar from './Sidebar/BrowserSidebar';
+import UserSidebar from './Sidebar/UserSidebar';
 import Content from './Content/Content';
 
 import './App.scss';
 
-const App: FC = () => {
-
-    return (
-        <div className="app-wrapper">
-            <SidebarLeft className="app-sidebar sidebar-left">
-                hello
-            </SidebarLeft>
-
-            <Header className="header" />
-            <Content className="content" />
-
-            <SidebarRight className="app-sidebar sidebar-right">
-                hello
-            </SidebarRight>
-        </div>
-    );
-}
+const App: FC = () => (
+    <div className="app-wrapper">
+        <BrowserSidebar className="app-sidebar sidebar-left"/>
+        <Header className="header" />
+        <Content className="content" />
+        <UserSidebar className="app-sidebar sidebar-right"/>
+    </div>
+);
 
 export default App;
