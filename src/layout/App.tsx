fdd1778c83738692223f1afc7b8e2a532dcaa539
radiium <remip.dev@gmail.com>
2019-09-29 13:02:09 +0200

    Feat/ rework layout

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,15 +1,71 @@
 import React, { FC } from 'react';
-import SidebarLeft from './Sidebar/SidebarLeft';
-import SidebarRight from './Sidebar/SidebarRight';
-import Content from './Content/Content';
-import './App.scss';
+import styled from 'styled-components'
+import Header from 'components/Header';
+import Sidebar from './Sidebar/Sidebar';
+import PictureViewer from 'components/PictureViewer/PictureViewer';
+import Modals from 'components/Modals';
+
+
+export const AppWrapper = styled.div`
+    margin: 0 auto;
+    padding: var(--gridMargin);
+    height: 100%;
+    max-height: 500px;
+    width: 100%;
+    max-width: 800px;
+`;
+
+export const AppContainer = styled.div`
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-row-gap: var(--gridMargin);
+    grid-column-gap: var(--gridMargin);
+    grid-template-rows: var(--rowHeight) calc(100% - var(--rowHeight) - var(--gridMargin));
+    grid-template-columns: repeat(2, 50%);
+`;
+
+export const AppHeader = styled.div`
+    grid-area: 1 / 1 / 2 / 3;
+    height: var(--rowHeight);
+    width: 100%;
+    background: rgba(0, 0, 0, .2);
+`;
+
+export const AppLeft = styled.div`
+    grid-area: 2 / 1 / 3 / 2;
+    height: 100%;
+    width: 100%;
+    background: rgba(0, 0, 0, .2);
+    overflow: hidden;
+    position: relative;
+`;
+
+export const AppRight = styled.div`
+    grid-area: 2 / 2 / 3 / 3;
+    height: 100%;
+    max-height: calc(100%);
+    width: 100%;
+    background: rgba(0, 0, 0, .2);
+`;
 
 const App: FC = () => (
-    <div className="app-wrapper">
-        <SidebarLeft className="app-sidebar sidebar-left"/>
-        <Content className="content" />
-        <SidebarRight className="app-sidebar sidebar-right"/>
-    </div>
+    <AppWrapper>
+    <AppContainer>
+        <AppHeader>
+        <Header />
+        </AppHeader>
+
+        <AppLeft>
+        <Sidebar />
+        <Modals />
+        </AppLeft>
+
+        <AppRight>
+        <PictureViewer />
+        </AppRight>
+    </AppContainer>
+    </AppWrapper>
 );
 
 export default App;
---
+++ src/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,69 @@
+import React, { Component, HTMLAttributes } from 'react';
+import styled from 'styled-components'
+import { RootState } from 'store/rootReducer';
+import { connect } from 'react-redux';
+import { Section, setUserSection, setBrowserSection } from 'store/sidebar/sidebar';
+import SidebarSection from 'components/SidebarSection';
+import { OpenLinkType } from 'store/ui/ui';
+
+
+export const SidebarWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+`;
+
+interface Props {
+    browserSection: any;
+    userSection: any;
+    isOnEdit: boolean;
+    openLink: OpenLinkType;
+    setBrowserSection: (section: Section) => void;
+    setUserSection: (section: Section) => void;
+}
+
+class Sidebar extends Component<Props & HTMLAttributes<HTMLDivElement>> {
+    render() {
+        const {
+            browserSection,
+            setBrowserSection,
+            userSection,
+            setUserSection,
+            isOnEdit } = this.props;
+
+        if (!userSection) {
+            return (null);
+        }
+
+        return (
+            <SidebarWrapper>
+                <SidebarSection
+                    isOnEdit={isOnEdit}
+                    section={browserSection}
+                    onSectionChange={setBrowserSection}
+                />
+                <SidebarSection
+                    isOnEdit={isOnEdit}
+                    section={userSection}
+                    onSectionChange={setUserSection}
+                />
+            </SidebarWrapper>
+        );
+    }
+}
+
+const mapStateToProps = (state: RootState) => {
+    return {
+        browserSection: state.sidebarState.browserSection,
+        userSection: state.sidebarState.userSection,
+        isOnEdit: state.sidebarState.isOnEdit,
+        openLink: state.uiState.openLink,
+    }
+};
+
+const mapDispatchToProps = (dispatch: any) => ({
+    setBrowserSection: (payload: Section) => dispatch(setBrowserSection(payload)),
+    setUserSection: (payload: Section) => dispatch(setUserSection({...payload})),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
