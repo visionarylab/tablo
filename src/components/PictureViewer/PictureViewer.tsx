8e5a968ea7d15cf21ad8dcfe2565d6dfe2005918
radiium <remip.dev@gmail.com>
2019-09-29 16:55:58 +0200

    Feat/ increase picture zoom factor

--- src/components/PictureViewer/PictureViewer.tsx
+++ src/components/PictureViewer/PictureViewer.tsx
@@ -47,7 +47,7 @@ class PictureViewer extends Component<Props & HTMLAttributes<HTMLDivElement>, St
             // enableGrab: false,
             // customSize: '100%'
             scaleBase: 0.9,
-            scaleExtra: 0.7
+            scaleExtra: 2
         });
         this.initSelectedPicture(this.props)
     }
