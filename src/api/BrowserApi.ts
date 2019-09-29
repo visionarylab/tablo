709698b58fcbab8fd1f7ee6010665bfb3463a57b
radiium <remip.dev@gmail.com>
2019-09-29 13:47:48 +0200

    Feat/ rework cross browser api

--- src/api/BrowserApi.ts
+++ src/api/BrowserApi.ts
@@ -11,36 +11,117 @@ if (typeof (window as any).browser === 'undefined' && ENV !== 'development') {
 }
 
 
-export default class BrowserApi {
+// export default class BrowserApi {
+//     static getBookmarksTree() {
+//         return ENV === 'development'
+//             ? Promise.resolve(require('./bookmarks.json'))
+//             : browser.bookmarks.getTree();
+//     }
+
+//     static openUrlInCurrentTab(url: string) {
+//         return ENV === 'development'
+//             ? Promise.resolve(window.open(url, '_self'))
+//             : browser.tabs.update({ url });
+//     }
+
+//     static openUrlInNewTab(url: string) {
+//         return ENV === 'development'
+//             ? Promise.resolve(window.open(url, '_blank'))
+//             : browser.tabs.create({ url });
+//     }
+
+//     static getFaviconUrl(url: string) {
+//         console.log('getFaviconUrl', url)
+//         if (ENV === 'development' || Utils.isFirefoxExtension()) {
+//             try {
+//                 const prefixLessUrl = new URL(url || "").hostname;
+//                 return `https://api.faviconkit.com/${prefixLessUrl}/40`;
+//             } catch (error) {
+//                 return '';
+//             }
+//         } else {
+//             return `chrome://favicon/size/16@2x/${url}`;
+//         }
+//     }
+// }
+
+
+
+
+class DevApi {
     static getBookmarksTree() {
-        return ENV === 'development'
-            ? Promise.resolve(require('./bookmarks.json'))
-            : browser.bookmarks.getTree();
+        return Promise.resolve(require('./bookmarks.json'));
     }
 
     static openUrlInCurrentTab(url: string) {
-        return ENV === 'development'
-            ? Promise.resolve(window.open(url, '_self'))
-            : browser.tabs.update({ url });
+        return Promise.resolve(window.open(url, '_self'));
     }
 
     static openUrlInNewTab(url: string) {
-        return ENV === 'development'
-            ? Promise.resolve(window.open(url, '_blank'))
-            : browser.tabs.create({ url });
+        return Promise.resolve(window.open(url, '_blank'));
     }
 
     static getFaviconUrl(url: string) {
-        console.log('getFaviconUrl', url)
-        if (ENV === 'development' || Utils.isFirefoxExtension()) {
-            try {
-                const prefixLessUrl = new URL(url || "").hostname;
-                return `https://api.faviconkit.com/${prefixLessUrl}/40`;
-            } catch (error) {
-                return '';
-            }
-        } else {
-            return `chrome://favicon/size/16@2x/${url}`;
+        try {
+            const prefixLessUrl = new URL(url || "").hostname;
+            return `https://api.faviconkit.com/${prefixLessUrl}/40`;
+        } catch (error) {
+            return '';
         }
     }
 }
+
+class FirefoxApi {
+    static getBookmarksTree() {
+        return browser.bookmarks.getTree();
+    }
+
+    static openUrlInCurrentTab(url: string) {
+        return browser.tabs.update({ url });
+    }
+
+    static openUrlInNewTab(url: string) {
+        return browser.tabs.create({ url });
+    }
+
+    static getFaviconUrl(url: string) {
+        try {
+            const prefixLessUrl = new URL(url || "").hostname;
+            return `https://api.faviconkit.com/${prefixLessUrl}/40`;
+        } catch (error) {
+            return '';
+        }
+    }
+}
+
+class ChromeApi {
+    static getBookmarksTree() {
+        return browser.bookmarks.getTree();
+    }
+
+    static openUrlInCurrentTab(url: string) {
+        return browser.tabs.update({ url });
+    }
+
+    static openUrlInNewTab(url: string) {
+        return browser.tabs.create({ url });
+    }
+
+    static getFaviconUrl(url: string) {
+        return `chrome://favicon/size/16@2x/${url}`;
+    }
+}
+
+let BrowserApi: any;
+if (ENV === 'development') {
+    BrowserApi = DevApi;
+} else {
+
+    if (Utils.isFirefoxExtension()) {
+        BrowserApi = FirefoxApi;
+    } else {
+        BrowserApi = ChromeApi;
+    }
+}
+
+export default BrowserApi;
